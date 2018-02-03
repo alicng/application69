07d8b962499a81616ca291d7f862e16fd3e8f598
alicng <integral38@hotmail.com>
2018-02-03 09:23:59 -0700

    application69

---
+++ src/javaapplication69/JavaApplication69.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication69;
+
+/**
+ *
+ * @author JW
+ */
+public class JavaApplication69 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+int dude []={2,3,4,5};
+int sum =0;
+for (int  x: dude)
+{
+    sum+=x;
+}
+      System.out.println(sum);  
+      int a=2;
+      int b=3;
+      System.out.print(a+b);
+ }}
+ 
+ 
+        
+        
+     
+   
+    
+
